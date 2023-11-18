b272f8099bb2bd6a5ae6e735358277b6e4ad4ea1
gshn <gshn@kakao.com>
2023-11-18 14:38:02 +0900

    feat: run all the time

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import { useState } from "react";
+import styles from "./App.module.css";
 import Button from "./Button";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1);
+  console.log("call an api");
   return (
     <div>
-      <h1 className="styles.title">Welcome back!!!</h1>
-      <Button text={"Continue"} />
+      <h1 className={styles.title}>{counter}</h1>
+      <Button text={"click me"} onClick={onClick} />
     </div>
   );
 }
