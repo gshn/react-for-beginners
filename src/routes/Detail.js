dbd237c5a395aa287a22369f8a12aec47e2af84e
gshn <gshn@kakao.com>
2023-11-18 19:08:02 +0900

    feat: movie detail

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,5 +1,24 @@
+import { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
 function Detail() {
-  return <h1>Detail</h1>
+  const { id } = useParams()
+  const [movie, setMovie] = useState([])
+  const getMovie = async () => {
+    const response = await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+    const json = await response.json()
+    console.log(json.data)
+    setMovie(json.data.movie)
+  }
+  useEffect(() => {
+    getMovie()
+  }, [])
+  return (
+    <div>
+      <img src={movie.large_cover_image} alt={movie.title} />
+      <h1>{movie.title}</h1>
+      <p>{movie.description_full}</p>
+    </div>
+  )
 }
 
 export default Detail
