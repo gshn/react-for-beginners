7a343fcc92fd8f1089ef53d382ef4bd1e23f4f75
gshn <gshn@kakao.com>
2023-11-18 15:22:11 +0900

    convention: delete semi colon

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
-import { useEffect, useState } from "react";
-import styles from "./App.module.css";
-import Button from "./Button";
+import { useEffect, useState } from "react"
+import styles from "./App.module.css"
+import Button from "./Button"
 
 function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
+  const [counter, setValue] = useState(0)
+  const [keyword, setKeyword] = useState("")
+  const onClick = () => setValue((prev) => prev + 1)
+  const onChange = (event) => setKeyword(event.target.value)
   useEffect(() => {
-    console.log("I run only once.");
+    console.log("I run only once.")
   }, [])
   useEffect(() => {
     console.log("I run when 'keyword' changes", keyword)
   }, [keyword])
   useEffect(() => {
-    console.log("I run when 'counter' changes.", counter);
+    console.log("I run when 'counter' changes.", counter)
   }, [counter])
   useEffect(() => {
-    console.log("I run when 'keyword' & 'counter' changes.", keyword, counter);
+    console.log("I run when 'keyword' or 'counter' changes.", keyword, counter)
   }, [keyword, counter])
   return (
     <div>
-      <input value={keyword} onChange={onChange} type="text" placeholder="Search here..." />
+      <input value={keyword} onChange={onChange} type="text" name="search" id="search" placeholder="Search here..." />
       <h1 className={styles.title}>{counter}</h1>
       <Button text={"click me"} onClick={onClick} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/Button.js
+++ src/Button.js
@@ -1,12 +1,12 @@
-import PropTypes from "prop-types";
-import styles from "./Button.module.css";
+import PropTypes from "prop-types"
+import styles from "./Button.module.css"
 
 function Button({ text, onClick }) {
-    return <button className={styles.btn} onClick={onClick}>{text}</button>;
+    return <button className={styles.btn} onClick={onClick}>{text}</button>
 }
 
 Button.propTypes = {
     text: PropTypes.string.isRequired,
-};
+}
 
-export default Button;
+export default Button
