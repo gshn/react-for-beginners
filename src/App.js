402f2b5eb220d28f305ef80e7de8c3b36e0024ba
gshn <gshn@kakao.com>
2023-11-18 15:10:44 +0900

    feat: useEffect

--- src/App.js
+++ src/App.js
@@ -4,13 +4,24 @@ import Button from "./Button";
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
   const onClick = () => setValue((prev) => prev + 1);
-  console.log("i run all the time");
+  const onChange = (event) => setKeyword(event.target.value);
   useEffect(() => {
-    console.log("CALL THE API...");
+    console.log("I run only once.");
   }, [])
+  useEffect(() => {
+    console.log("I run when 'keyword' changes", keyword)
+  }, [keyword])
+  useEffect(() => {
+    console.log("I run when 'counter' changes.", counter);
+  }, [counter])
+  useEffect(() => {
+    console.log("I run when 'keyword' & 'counter' changes.", keyword, counter);
+  }, [keyword, counter])
   return (
     <div>
+      <input value={keyword} onChange={onChange} type="text" placeholder="Search here..." />
       <h1 className={styles.title}>{counter}</h1>
       <Button text={"click me"} onClick={onClick} />
     </div>
