0b33344b0917ccc8abb454a8cf36aa2282008b67
gshn <gshn@kakao.com>
2023-11-18 17:25:05 +0900

    feat: components

--- src/App.js
+++ src/App.js
@@ -1,38 +1,7 @@
-import { useEffect, useState } from "react"
+import Home from "./routes/Home"
 
 function App() {
-  const [loading, setLoading] = useState(true)
-  const [movies, setMovies] = useState([])
-  const getMovies = async() => {
-    const response = await fetch("https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year")
-    const json = await response.json()
-    setMovies(json.data.movies)
-    setLoading(false)
-  }
-  useEffect(() => {
-    getMovies()
-  }, [])
-  return (
-    <div>
-      <h1>The Movies!({loading ? 0 : movies.length})</h1>
-      {loading ? <strong>Loading...</strong> : (
-        <div>
-          {movies.map((movie) =>
-            <div key={movie.id}>
-              <img src={movie.medium_cover_image} alt={movie.title} />
-              <h2>{movie.title}({movie.rating})</h2>
-              <p>{movie.summary}</p>
-              <ul>
-                {movie.genres.map((g) => (
-                  <li key={g}>{g}</li>
-                ))}
-              </ul>
-            </div>
-          )}
-        </div>
-      )}
-    </div>
-  )
+  return <Home />
 }
 
 export default App
