45ae71f657d1c04dd6229fc7aaa35e9e291fd2c7
gshn <gshn@kakao.com>
2023-11-18 14:51:44 +0900

    feat: useEffect

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "./App.module.css";
 import Button from "./Button";
 
 function App() {
   const [counter, setValue] = useState(0);
   const onClick = () => setValue((prev) => prev + 1);
-  console.log("call an api");
+  console.log("i run all the time");
+  useEffect(() => {
+    console.log("CALL THE API...");
+  }, [])
   return (
     <div>
       <h1 className={styles.title}>{counter}</h1>
